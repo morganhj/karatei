c1b5ba160d934dba93d8b144b8860a78cff9f242
Morgan Hoare <hoaremorgan@gmail.com>
2020-07-05 03:29:44 -0300

    fix current_unsigned user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-  	@sources = current_user.list.sources
+  	@sources = current_user.list.sources if current_user
   end
 end
